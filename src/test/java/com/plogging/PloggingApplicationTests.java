3357b4006e36cb1b9cb2ceadf1ee76a9390aeeb1
kyubeom <be_man@naver.com>
2022-07-09 16:45:58 +0900

    initial commit

---
+++ src/test/java/com/plogging/PloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
