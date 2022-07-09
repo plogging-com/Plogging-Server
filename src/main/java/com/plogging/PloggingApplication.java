44de8641b4de4bc728b32831b9807c8163ee44f6
kyubeom <be_man@naver.com>
2022-07-09 16:45:58 +0900

    initial commit

---
+++ src/main/java/com/plogging/PloggingApplication.java
@@ -0,0 +1,16 @@
+package com.plogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloggingApplication {
+	/*
+	깃테스트 ㅇㅇ
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloggingApplication.class, args);
+	}
+
+}
