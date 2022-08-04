e4ac034c9ea8d133d085fb082d193f42c62f97b0
MAVOTUNA <sdm03131229@gmail.com>
2022-08-04 14:13:04 +0900

    #37 git rollback2

--- src/main/java/com/plogging/domain/User/service/UserServiceImpl.java
+++ src/main/java/com/plogging/domain/User/service/UserServiceImpl.java
@@ -2,11 +2,9 @@ package com.plogging.domain.User.service;
 
 import com.plogging.domain.User.dto.*;
 import com.plogging.domain.User.entity.User;
-import com.plogging.domain.User.entity.UserRefreshToken;
 import com.plogging.domain.User.exception.NotFoundUserException;
 import com.plogging.domain.User.exception.UserIdDuplicationException;
 import com.plogging.domain.User.repository.UserRepository;
-import com.plogging.global.jwt.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,11 +15,6 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final JwtService jwtService;
-
-    private final UserRefreshTokenService userRefreshTokenService;
-
-
 
     @Override
     public UserJoinRes join(UserJoinReq userJoinReq) {
@@ -37,18 +30,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserLoginRes login(UserLoginReq userLoginReq) {
-
         User user = userRepository.findByLoginId(userLoginReq.getLoginId()).orElseThrow(NotFoundUserException::new);
+        return null;
 
-        String accessJwt = jwtService.createAccessJwt(user.getLoginId());
-        String refreshJwt = jwtService.createRefreshJwt(user.getLoginId());
-
-        long refreshTokenIdx = userRefreshTokenService.insertRefreshToken(refreshJwt, user);
-
-        return  UserLoginRes.builder()
-                .name(user.getName())
-                .accessToken(accessJwt)
-                .refreshTokenIdx(refreshTokenIdx).build();
     }
 
 
