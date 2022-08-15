58d9e249adde7d8a9d66c42ad66de23de6abda55
kingchan223 <kokiyo97@naver.com>
2022-08-15 21:28:55 +0900

    #24 PLOG-9 QuestController에 @ApiOperation 추가

--- src/main/java/com/plogging/domain/Quest/service/quest/QuestServiceImpl.java
+++ src/main/java/com/plogging/domain/Quest/service/quest/QuestServiceImpl.java
@@ -13,6 +13,7 @@ import com.plogging.domain.Quest.repository.QuestProceedingRepository;
 import com.plogging.domain.Quest.repository.QuestRepository;
 import com.plogging.domain.User.entity.User;
 import com.plogging.global.dto.ApplicationResponse;
+import com.plogging.global.utill.imgae.AwsS3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,7 +30,7 @@ public class QuestServiceImpl implements QuestService{
     private final QuestRepository questRepository;
     private final QuestProceedingRepository questProceedingRepository;
     private final QuestCompleteRepository questCompleteRepository;
-    // private final S3Service s3Service; //TODO
+    private final AwsS3Service s3Service;
 
     @Transactional
     @Override
