63cb090b5d1533b892382dab90af858105356625
shinhn <rkdrpgml@naver.com>
2022-08-18 00:16:23 +0900

    #27 PLOG-11 게시글 글자수 700자 예외처리

--- src/main/java/com/plogging/domain/Board/service/board/BoardServiceImpl.java
+++ src/main/java/com/plogging/domain/Board/service/board/BoardServiceImpl.java
@@ -4,17 +4,15 @@ import com.plogging.domain.Board.dto.board.request.createBoardReq;
 import com.plogging.domain.Board.dto.board.response.BoardListRes;
 import com.plogging.domain.Board.dto.board.response.BoardRes;
 import com.plogging.domain.Board.entity.Board;
+import com.plogging.domain.Board.exception.Board.OverMaxContentLength;
 import com.plogging.domain.Board.repository.BoardRepository;
-import com.plogging.domain.Board.repository.CommentRepository;
-import com.plogging.domain.Board.repository.HeartRepository;
 import com.plogging.domain.User.entity.User;
+import com.plogging.domain.User.exception.NotFoundUserException;
 import com.plogging.domain.User.repository.UserRepository;
 import com.plogging.global.dto.ApplicationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,10 +27,12 @@ public class BoardServiceImpl implements BoardService{
     @Transactional
     @Override
     public ApplicationResponse<BoardRes> createBoard(createBoardReq createBoardReq) {
+        // 글자수가 700자를 넘는 경우 에러
+        if(createBoardReq.getContent().length() > 700) throw new OverMaxContentLength();
+
         String imageURL = "~~"; // s3Service.makeImage(questReq.getPhoto());
 
-        User user = userRepository.findById(createBoardReq.getUser_idx()).get();
-
+        User user = userRepository.findById(createBoardReq.getUserId()).orElseThrow(() -> new NotFoundUserException());
         Board board = boardRepository.save(createBoardReq.toEntityWithPhoto(imageURL, user));
 
         BoardRes boardRes = BoardRes.create(board);
