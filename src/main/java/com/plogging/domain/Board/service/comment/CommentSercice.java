d1207fea62af5fad9989f90a33ba74364668f37d
shinhn <rkdrpgml@naver.com>
2022-08-20 01:37:34 +0900

    #71 PLOG-27 게시글 댓글 수정 기능

--- src/main/java/com/plogging/domain/Board/service/comment/CommentSercice.java
+++ src/main/java/com/plogging/domain/Board/service/comment/CommentSercice.java
@@ -1,6 +1,7 @@
 package com.plogging.domain.Board.service.comment;
 
 import com.plogging.domain.Board.dto.board.request.createCommentReq;
+import com.plogging.domain.Board.dto.board.request.modifyCommentReq;
 import com.plogging.domain.Board.dto.board.response.CommentRes;
 import com.plogging.global.dto.ApplicationResponse;
 
@@ -8,4 +9,6 @@ public interface CommentSercice {
     ApplicationResponse<CommentRes> createComment(createCommentReq createCommentReq);
 
     ApplicationResponse<CommentRes> delComment(Long commentId);
+
+    ApplicationResponse<CommentRes> modifyComment(modifyCommentReq modifyCommentReq);
 }
