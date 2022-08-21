babfec35bc161e6cfa9d0926bc0f96d9f99f8d86
kingchan223 <kokiyo97@naver.com>
2022-08-21 22:11:32 +0900

    #25 PLOG-9 quest-proceeding. 진행중인 퀘스트 전체 조회 수정.

--- src/main/java/com/plogging/domain/Quest/repository/QuestProceedingRepository.java
+++ src/main/java/com/plogging/domain/Quest/repository/QuestProceedingRepository.java
@@ -1,7 +1,11 @@
 package com.plogging.domain.Quest.repository;
 
 import com.plogging.domain.Quest.entity.UserQuestProceeding;
+import com.plogging.domain.User.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestProceedingRepository extends JpaRepository<UserQuestProceeding, Long> {
+    Page<UserQuestProceeding> findAllByUser(Pageable pageable, User user);
 }
