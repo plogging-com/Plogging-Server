6b8c4d94e8d2df29e0fb7aa5e1092d29b3e90e19
kingchan223 <kokiyo97@naver.com>
2022-08-21 22:46:20 +0900

    #25 PLOG-9 quest-complete. 완료 quest 전체 조회 추가.

--- src/main/java/com/plogging/domain/Quest/dto/userQuestComplete/response/QuestCompRes.java
+++ src/main/java/com/plogging/domain/Quest/dto/userQuestComplete/response/QuestCompRes.java
@@ -1,4 +1,26 @@
 package com.plogging.domain.Quest.dto.userQuestComplete.response;
 
+import com.plogging.domain.Quest.entity.UserQuestComplete;
+import com.plogging.domain.Quest.entity.UserQuestProceeding;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class QuestCompRes {
+    private String name;
+    private String imageUrl;
+    private int level;
+    private String endTime;
+
+    public static QuestCompRes create(UserQuestComplete userQuestComplete) {
+        QuestCompRes questCompRes = new QuestCompRes();
+        questCompRes.name = userQuestComplete.getQuest().getName();
+        questCompRes.imageUrl = userQuestComplete.getQuest().getPhoto(); //TODO
+        questCompRes.level = userQuestComplete.getLevel();
+        questCompRes.endTime = userQuestComplete.getEndTime().toString();
+        return questCompRes;
+    }
 }
