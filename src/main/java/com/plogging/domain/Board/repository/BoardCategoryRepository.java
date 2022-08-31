42c230923c0c7fbd053060934ee2fb7d9577d716
kyubeom <be_man@naver.com>
2022-08-31 12:13:46 +0900

    #90 PLOG-29 게시글 카테고리 쿼리 추가

--- src/main/java/com/plogging/domain/Board/repository/BoardCategoryRepository.java
+++ src/main/java/com/plogging/domain/Board/repository/BoardCategoryRepository.java
@@ -6,10 +6,19 @@ import org.apache.el.stream.Stream;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface BoardCategoryRepository extends JpaRepository<BoardCategory, Long> {
     Page<BoardCategory> findByCategory(Pageable pageable, Category category);
-    Page<BoardCategory> findAllByCategoryIn(Pageable pageable, List<Category> categories);
+
+    @Query(value = "select distinct (board.board_idx), comment_cnt, content, heart_cnt, status, time, title, user_idx, main_photo_url " +
+            " from  board, board_category, category" +
+            " where board.board_idx = board_category.board_idx" +
+            "        and board_category.category_idx = category.category_idx" +
+            "        and category.name IN (:param1)", nativeQuery = true)
+    Page<BoardCategory> findAllByCategoryIn(Pageable pageable, List<Category> categories, String param1);
+
+
 }
