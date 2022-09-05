dcd777305120b78dd5f8534d39e856eaf94c6191
kingchan223 <kokiyo97@naver.com>
2022-09-05 11:03:29 +0900

    #24 PLOG-9 에러 수정

--- src/main/java/com/plogging/domain/Quest/controller/QuestDiaryController.java
+++ src/main/java/com/plogging/domain/Quest/controller/QuestDiaryController.java
@@ -22,7 +22,7 @@ public class QuestDiaryController{
     private final QuestDiaryService questDiaryService;
     //
     @ApiOperation(value = "Quest Diary 작성하기", notes = "Quest Diary 작성.")
-    @PostMapping("/{quest_id}")
+    @PostMapping("/quest/{quest_id}")
     public ApplicationResponse<QuestDiaryDeatilResp> create(
                                             @PathVariable Long quest_id,
                                             @ModelAttribute QuestDiaryReq questDiaryReq){
